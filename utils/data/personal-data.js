86ed3d78248da7e8d2759ed5045ad86dd3a2fbac
software-dev-000 <davidann1227@gmail.com>
2024-10-23 01:58:20 +0900

    backup bio title

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,7 +1,7 @@
 export const personalData = {
   name: "Victor Martinez",
   profile: '/profile.png',
-  designation: "Python Engineer",
+  designation: "Python Machine Learning and Data Engineer",
   description: "My name is Victor Martinez. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on Python and I love to do most of the things using Python. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'victor91319@gmail.com',
   phone: '+1 765 331 1950',
